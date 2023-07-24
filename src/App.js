6f831817716d8b7b23a9d736cbea609485f85305
Fish Chan <>
2023-07-24 14:05:00 +0800

    Setting Home Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
+import React from "react";
+import Home from "../components/home/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing Hello Word!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />
 }
 
 export default App;
